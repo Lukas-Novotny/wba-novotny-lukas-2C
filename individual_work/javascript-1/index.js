6dcb9fd6a15fd9b0fb29fe18a81affb5e5961729
Lukas-Novotny <luky-novotny@post.cz>
2022-04-28 13:29:23 +0200

    dsds

--- individual_work/javascript-1/index.js
+++ individual_work/javascript-1/index.js
@@ -1,1 +1,12 @@
-alert("hello world")
+
+function bachaFunction(){
+    var div = document.getElementById('idk')
+    var bacha = document.createElement("div");
+    bacha.setAttribute('class', 'bacha');
+    console.log(div.hasChildNodes())
+if(div.hasChildNodes() == false){
+
+div.appendChild(bacha);
+
+}
+}
