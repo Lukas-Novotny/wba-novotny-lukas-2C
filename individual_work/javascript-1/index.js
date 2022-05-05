ba9bcc7244018b5a510d132d8b290ec1f7a15984
Lukas-Novotny <luky-novotny@post.cz>
2022-05-05 13:03:33 +0200

    uijikk

--- individual_work/javascript-1/index.js
+++ individual_work/javascript-1/index.js
@@ -1,1 +1,20 @@
-alert("hello world")
+var txt = document.getElementById("text")
+var notVisible = document.getElementsByClassName("visiblent")
+
+// function toggle(){
+//     if(notVisible.length>0){
+//         txt.setAttribute("class","visible")
+//     }
+//     else{
+//         txt.setAttribute("class","visiblent")
+//     }
+//     console.log(notVisible.length)
+// }
+ 
+function toggle() {
+    var element = document.getElementById("text");
+    element.classList.toggle("visiblent");
+    element.classList.toggle("text");
+
+  }
+  
